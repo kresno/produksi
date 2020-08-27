c65e1d0ecaa90843f5e2fabd3f9bb351cf4b53fc
Kresno Satrio Kuncoro Zito <kresno.19@gmail.com>
2020-08-27 20:01:57 +0700

    first commit

---
+++ application/views/ikm/template/_foot.php
@@ -0,0 +1,18 @@
+        <!-- jQuery  -->
+        <script src="<?php echo base_url(); ?>public/assets/js/jquery.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/js/metisMenu.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/js/waves.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/js/jquery.slimscroll.min.js"></script>
+
+        <script src="<?php echo base_url(); ?>public/assets/plugins/moment/moment.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/plugins/apexcharts/apexcharts.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+        <script src="<?php echo base_url(); ?>public/assets/pages/jquery.eco_dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="<?php echo base_url(); ?>public/assets/js/app.js"></script>
+
+    </body>
+</html>
---
+++ application/views/ikm/template/_head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>IKM Database</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Web Database IKM Kabupaten Sukabumi" name="description" />
+        <meta content="Kabupaten Sukabumi" name="author" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="<?php echo base_url(); ?>public/assets/images/favicon.ico">
+
+        <link href="<?php echo base_url(); ?>public/assets/plugins/jvectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet">
+
+        <!-- App css -->
+        <link href="<?php echo base_url(); ?>public/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url(); ?>public/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url(); ?>public/assets/css/metisMenu.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url(); ?>public/assets/css/style.css" rel="stylesheet" type="text/css" />
+
+    </head>
